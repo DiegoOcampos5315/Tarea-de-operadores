fffd42881a9aeb4bf145dd45f9068e9b25826643
DiegoOcampos5315 <131500011+DiegoOcampos5315@users.noreply.github.com>
2023-04-23 19:57:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+let mayor;
+if (num2 > num1){
+   mayor = num2;
+} else{
+    mayor = num1;
+}
+
+if (num3 > num2){
+    mayor = num3;
+}else {
+    mayor = num2;
+}
+
+    console.log (mayor)
+
